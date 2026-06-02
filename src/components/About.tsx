5a138bfd0b8ae371a322a7f6ebfc077fe748b07b
Rocolusso <rocolusso@icloud.com>
2026-06-02 21:02:27 +0300

    fixed About sizes

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -4,7 +4,7 @@ import aboutBg from "./../../public/img/about_bg.jpg"
 
 const About = () => {
     return (
-        <section className="relative w-full h-[500px] overflow-hidden">
+        <section className="relative w-full h-[417px] overflow-hidden">
             {/* Background image */}
             <Image
                 src={aboutBg}
@@ -14,12 +14,12 @@ const About = () => {
                 className="object-cover"
             />
             <div className="absolute inset-0 bg-black/50" />
-            <div className="container mx-auto relative z-10 pt-47">
+            <div className="container mx-auto relative z-10 pt-35">
                 <div className="max-w-6xl px-6 text-white ">
-                    <h1 className="text-6xl font-bold ">
+                    <h1 className="text-[3.25rem] font-bold ">
                         About Us
                     </h1>
-                    <p className="max-w-2xl text-[1.4rem] text-white/90 pt-5">
+                    <p className="max-w-2xl text-[1.125rem] text-white max-w-[540px]">
                         Lorem ipsum dolor sit amet. Id quasi assumenda non excepturi
                         rerum sed quia quasi sit velit ducimus! Quo quod nesciunt aut
                         omnis praesentium quo dignissimos reiciendis sit numquam delectus.
