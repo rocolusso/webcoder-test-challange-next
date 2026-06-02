5299b81a06845f4c218ab2abf7f5fed37a459ab7
Rocolusso <rocolusso@icloud.com>
2026-06-02 22:43:25 +0300

    fixed Servisec sizes / added Poppins fonts

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -10,19 +10,21 @@ const Services = () => {
             <div className={'container mx-auto'}>
                  <div className={'flex justify-center flex-col'}>
 
-                     <div className='item_block py-20 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-20'>
+                     <div className='item_block py-20 flex flex-row flex-wrap items-center gap-18' >
                          <div className={'flex justify-center'}>
-                             <Image
-                                 className={'item_img'}
-                                 src={serviceItemImg}
-                                 alt="service-item-1"
-                                 width={550}
-                                 height={550}
-                             />
+                             <div className={'pl-10'}>
+                                 <Image
+                                     className={'item_img'}
+                                     src={serviceItemImg}
+                                     alt="service-item-1"
+                                     width={532}
+                                     height={545}
+                                 />
+                             </div>
                          </div>
-                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[400px]'}>
-                              <h3 className={'text-4xl font-bold capitalize '}>Lorem Ipsum</h3>
-                              <p className={'pt-10'}>
+                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[560px]'}>
+                              <h3 className={'capitalize services_heading'}>Lorem Ipsum</h3>
+                              <p className={'pt-6 services_heading_paragraph'}>
                                   Lorem ipsum dolor sit amet. Id quasi assumenda non excepturi rerum sed quia quasi
                                   sit velit ducimus! Quo quod nesciunt aut omnis praesentium quo dignissimos
                                   reiciendis sit numquam delectus.
@@ -30,38 +32,42 @@ const Services = () => {
                          </div>
                      </div>
 
-                     <div className='item_block py-20 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-20'>
-                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[400px]'}>
-                             <p>
+                     <div className='item_block py-5 flex flex-row flex-wrap items-center gap-18' >
+                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[560px]'}>
+                             <p className={'pt-6 services_heading_paragraph'}>
                                  Lorem ipsum dolor sit amet. Id quasi assumenda non excepturi rerum sed quia quasi
                                  sit velit ducimus! Quo quod nesciunt aut omnis praesentium quo dignissimos
                                  reiciendis sit numquam delectus.
                              </p>
                          </div>
-
                          <div className={'flex justify-center'}>
-                             <Image
-                                 className={'item_img'}
-                                 src={aboutBg}
-                                 alt="service-item-1"
-                                 width={550}
-                                 height={550}
-                             />
+                             <div className={'ml-5'}>
+                                 <Image
+                                     className={'item_img'}
+                                     src={aboutBg}
+                                     alt="service-item-1"
+                                     width={540}
+                                     height={540}
+                                 />
+                             </div>
                          </div>
                      </div>
 
-                     <div className='item_block py-20 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-20'>
+
+                     <div className='item_block py-20 flex flex-row flex-wrap items-center gap-18' >
                          <div className={'flex justify-center'}>
-                             <Image
-                                 className={'item_img'}
-                                 src={aboutBg}
-                                 alt="service-item-1"
-                                 width={550}
-                                 height={550}
-                             />
+                             <div className={'ml-10'}>
+                                 <Image
+                                     className={'item_img'}
+                                     src={aboutBg}
+                                     alt="service-item-1"
+                                     width={540}
+                                     height={540}
+                                 />
+                             </div>
                          </div>
-                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[400px]'}>
-                             <p>
+                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[560px]'}>
+                             <p className={'pt-6 services_heading_paragraph'}>
                                  Lorem ipsum dolor sit amet. Id quasi assumenda non excepturi rerum sed quia quasi
                                  sit velit ducimus! Quo quod nesciunt aut omnis praesentium quo dignissimos
                                  reiciendis sit numquam delectus.
@@ -69,44 +75,38 @@ const Services = () => {
                          </div>
                      </div>
 
-                     <div className='item_block py-20 mx-auto grid grid-cols-1 sm:grid-cols-2 gap-20'>
-                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[400px]'}>
-                             <p>
+                     <div className='item_block py-5 flex flex-row flex-wrap items-center gap-18' >
+                         <div className={'px-5 item_description text-white flex flex-col justify-center w-full sm:max-w-[560px]'}>
+                             <p className={'pt-6 services_heading_paragraph'}>
                                  Lorem ipsum dolor sit amet. Id quasi assumenda non excepturi rerum sed quia quasi
                                  sit velit ducimus! Quo quod nesciunt aut omnis praesentium quo dignissimos
                                  reiciendis sit numquam delectus.
                              </p>
                          </div>
-
-                        <div className={'flex justify-center'}>
-                            <Image
-                                className={'item_img'}
-                                src={aboutBg}
-                                alt="service-item-1"
-                                width={550}
-                                height={550}
-                            />
-                        </div>
+                         <div className={'flex justify-center'}>
+                             <div className={'ml-5'}>
+                                 <Image
+                                     className={'item_img'}
+                                     src={aboutBg}
+                                     alt="service-item-1"
+                                     width={540}
+                                     height={540}
+                                 />
+                             </div>
+                         </div>
                      </div>
 
                  </div>
-                <div className={'container mx-auto'}>
-                    <div className={'border border-white'}></div>
+                <div className={'border border-white mt-20 overflow-hidden'}/>
+                <div className={'py-20 flex flex-col items-center'}>
+                    <Image
+                        className={'video_img'}
+                        src={videoImg}
+                        alt="video-img"
+                        width={980}
+                        height={980}
+                    />
                 </div>
-                <div className={'container mx-auto'}>
-                    <div className={'py-20 flex justify-center'}>
-                        <Image
-                            className={'video_img'}
-                            src={videoImg}
-                            alt="video-img"
-                            width={980}
-                            height={980}
-                        />
-                    </div>
-                </div>
-
-
-
 
             </div>
         </section>
